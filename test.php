65c3945975054a85d20b7ed59e18b6d0173b30f0
Scott <smcnalty@uwo.ca>
2017-05-17 14:27:19 -0400

    Up to end of PHP and Laravel

---
+++ test.php
@@ -0,0 +1,17 @@
+<?php
+
+class Person {
+
+  public $name = 'chris';
+  public $username = 'chrisoncode';
+
+  public function doNinjaKick($type = 'front') {
+
+  }
+
+}
+
+$chris = new Person;
+
+$chris->doNinjaKick();
+
